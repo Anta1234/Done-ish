c1ba3649ae65a5804fc7f80d1978efd96a32740f
Anta1234 <89853107+Anta1234@users.noreply.github.com>
2022-01-14 16:59:51 -0500

    Initial commit

---
+++ src/components/product.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Product(props) {
+  return (
+    <div className="Product">
+      <h2>{props.name}</h2>
+      <h3>{props.price}</h3>
+      <div className="buttons">
+        <button
+          onClick={() => {
+            props.addToShoppingCart(props.name);
+          }}
+        >
+          Add
+        </button>
+        <button
+          onClick={() => {
+            props.removeFromShoppingCart(props.name);
+            // props.removeFromShoppingCart(props.price);
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
